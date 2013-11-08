3262c6abef28d9db6a952cfcc99cd798efbe57f9
Tom Aldcroft <taldcroft@gmail.com>
2013-11-08 13:33:55 -0500

    Update responsivity.py for more visually informative plot

--- responsivity/responsivity.py
+++ responsivity/responsivity.py
@@ -1,24 +1,56 @@
-from Ska.DBI import DBI
-from Chandra.Time import DateTime
+"""
+Plot the delta magnitude histories of the 4 stars which have been tracked more than N
+(300) times.
+
+>>> run -i responsivity
+>>> repeats = get_repeats()
+>>> plot_repeat_star_mags(repeats)
+"""
+
+import numpy as np
 import matplotlib.pyplot as plt
 
-# plot the delta magnitude histories of the 4 stars which have been tracked more than N (300) times
+from Ska.DBI import DBI
+from Chandra.Time import DateTime
 
-def plot_repeat_star_mags():
+
+cache = {}
+
+
+def plot_repeat_star_mags(repeats):
+    plt.close(1)
+    plt.figure(1, figsize=(6, 4))
+
     aca_db = DBI(server='sybase', dbi='sybase', user='aca_read')
-    n_repeats = 300 # gives us 4 stars
-    repeats = aca_db.fetchall("select id, count(id) as num_obs from trak_stats_data group by id having (count(id) > {})".format(n_repeats))
-    plt.figure(figsize=(6, 3))
     for agasc_id in repeats['id']:
-        if agasc_id < 20:
-            continue
         print agasc_id
-        obsdata = aca_db.fetchall("select * from trak_stats_data where id = {} order by kalman_tstart".format(agasc_id))
-        plt.plot(DateTime(obsdata['kalman_tstart']).frac_year, obsdata['mag_exp'] - obsdata['aoacmag_mean'], '.')
+        if agasc_id in cache:
+            obsdata = cache[agasc_id]
+        else:
+            obsdata = aca_db.fetchall("select * from trak_stats_data where id = {} "
+                                      "order by kalman_tstart".format(agasc_id))
+            cache[agasc_id] = obsdata
+        years = DateTime(obsdata['kalman_tstart']).frac_year
+        scatter = np.random.uniform(-0.5, 0.5, size=len(years))
+        dmags = obsdata['aoacmag_mean'] - np.median(obsdata['aoacmag_mean'])
+        plt.plot(years + scatter, dmags, '.', label='ID {}'.format(agasc_id))
+    aca_db.conn.close()
+
     plt.xlabel('Year')
-    plt.ylabel('Catalog Mag - Observed Mag')
+    plt.ylabel('Delta Mag')
     plt.grid()
-    plt.ylim(-0.17, 0.17)
+    plt.ylim(-0.1, 0.1)
+    plt.legend(loc='upper left', fontsize=10)
+    plt.title('ACA Responsivity')
     plt.tight_layout()
     plt.savefig('responsivity.png')
 
+
+def get_repeats(n_repeats=300):  # default gives us 4 stars
+    aca_db = DBI(server='sybase', dbi='sybase', user='aca_read')
+    repeats = aca_db.fetchall("select id, count(id) as num_obs from trak_stats_data "
+                              "group by id having (count(id) > {})".format(n_repeats))
+    repeats = repeats[repeats['id'] >= 20]
+    aca_db.conn.close()
+
+    return repeats
