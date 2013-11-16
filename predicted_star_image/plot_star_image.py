d093efd163bcbaef2a7893d48c9fdddd261b453c
Tom Aldcroft <taldcroft@gmail.com>
2013-11-16 12:11:45 -0500

    Remove pyplot *

--- predicted_star_image/plot_star_image.py
+++ predicted_star_image/plot_star_image.py
@@ -1,4 +1,4 @@
-from matplotlib.pyplot import *
+import matplotlib.pyplot as plt
 from matplotlib import cm
 from astropy.io import fits
 
@@ -15,42 +15,44 @@ dark_img = dark[400:440, 400:440]  # e-/sec from calibration
 dark_t0 = -15.31
 star_mag = 10.3
 
-rc("axes", labelsize=10, titlesize=10)
-rc("xtick", labelsize=10)
-rc("ytick", labelsize=10)
-rc("font", size=10)
+plt.rc("axes", labelsize=10, titlesize=10)
+plt.rc("xtick", labelsize=10)
+plt.rc("ytick", labelsize=10)
+plt.rc("font", size=10)
+
 
 def draw_star(t=-16.0, mag=10.3):
-    clf()
+    plt.clf()
     img = dark_img.copy() * 10 ** ((t - dark_t0) / 21.0)
     nn = dark_img.shape[0] / 2
-    img[nn -3:nn + 3, nn -3:nn + 3] += star_img * 10 ** ((star_mag - mag) * 0.4)
+    img[nn - 3:nn + 3, nn - 3:nn + 3] += star_img * 10 ** ((star_mag - mag) * 0.4)
 
-    imshow(img, interpolation='nearest', cmap=cm.gray)
-    colorbar()
+    plt.imshow(img, interpolation='nearest', cmap=cm.gray)
+    plt.colorbar()
 
 
 def draw_star_dark_range():
-    figure(figsize=(3.5, 3.5))
+    plt.figure(figsize=(3.5, 3.5))
     for t in [-15, -10, -5, 0]:
         for mag in [10.6, 10.0]:
             draw_star(t, mag)
-            title('T = {} degC, Mag_ACA = {}'.format(t, mag))
-            ax = gca()
+            plt.title('T = {} degC, Mag_ACA = {}'.format(t, mag))
+            ax = plt.gca()
             ax.xaxis.set_visible(False)
             ax.yaxis.set_visible(False)
-            tight_layout()
-            savefig('star_img_mag{}_T{}.png'.format(mag, t))
+            plt.tight_layout()
+            plt.savefig('star_img_mag{}_T{}.png'.format(mag, t))
+
 
 def draw_star_dark2000():
-    figure(figsize=(3.5, 3.5))
+    plt.figure(figsize=(3.5, 3.5))
     t = -20.3
     mag = 10.6
 
     draw_star(t, mag)
-    title('T = -10 degC, Mag_ACA = {}'.format(t, mag))
-    ax = gca()
+    plt.title('T = -10 degC, Mag_ACA = {}'.format(t, mag))
+    ax = plt.gca()
     ax.xaxis.set_visible(False)
     ax.yaxis.set_visible(False)
-    tight_layout()
-    savefig('star_img_2000_mag{}_T{}.png'.format(mag, t))
+    plt.tight_layout()
+    plt.savefig('star_img_2000_mag{}_T{}.png'.format(mag, t))
